cbdc93af473f5050d4ee522387dd2046dcab5b4a
Saharat <saharat.sa.68@ubu.ac.th>
2026-02-07 01:20:21 +0700

    remove misspelled bollot.py

--- bollot.py
+++
@@ -1,8 +0,0 @@
-def save_vote(student_id, choice):
-    # Role 4: เขียนระบบบันทึกคะแนนลงไฟล์หรือ List
-    try:
-        with open("votes.txt", "a") as f:
-            f.write(f"{student_id}:{choice}\n")
-        return True
-    except:
-        return False
